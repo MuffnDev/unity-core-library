0e97f1202284b8c102fa8b85b38851542e36c506
Hubert GROSSIN <hubert.grossin@gmail.com>
2020-10-27 01:12:41 +0100

    Add SelectionList (missing docs)

---
+++ Editor/Demos/SelectionListDemoEditor.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace MuffinDev.EditorUtils.Demos
+{
+
+	///<summary>
+	/// This editor window demonsttrates how to use the SelectionList Editor GUI utility.
+	///</summary>
+	public class SelectionListDemoEditor : EditorWindow
+	{
+
+		#region Properties
+
+		private const string EDITOR_WINDOW_MENU_ITEM = "Tools/Muffin Dev/Demos/Selection List";
+		private const string EDITOR_WINDOW_TITLE = "Selection List";
+		private const int EDITOR_WINDOW_PRIORITY = 1;
+
+		[SerializeField]
+		private string[] m_Items = { };
+
+		[SerializeField]
+		private SelectionList m_SelectionList = null;
+
+		[SerializeField]
+		private Vector2 m_ScrollPosition = Vector2.zero;
+
+		private SerializedProperty m_ItemsProp = null;
+
+		#endregion
+
+
+		#region Lifecycle
+
+		private void OnEnable()
+		{
+			if (m_Items == null || m_Items.Length == 0)
+			{
+				m_Items = new string[] { "Apple", "Banana", "Lemon" };
+			}
+			m_SelectionList = new SelectionList(m_Items);
+
+			SerializedObject obj = new SerializedObject(this);
+			m_ItemsProp = obj.FindProperty(nameof(m_Items));
+		}
+
+		#endregion
+
+
+		#region GUI
+
+		private void OnGUI()
+		{
+			m_ScrollPosition = EditorGUILayout.BeginScrollView(m_ScrollPosition, GUILayout.Height(120f));
+			{
+				m_SelectionList.Items = new List<string>(m_Items);
+				EditorGUILayout.PropertyField(m_ItemsProp, true);
+				m_ItemsProp.serializedObject.ApplyModifiedProperties();
+			}
+			EditorGUILayout.EndScrollView();
+
+			EditorGUILayout.Space();
+			m_SelectionList.DrawLayout();
+		}
+
+		/// <summary>
+		/// Gets the window of this tool, or create it if it's not already open.
+		/// </summary>
+		[MenuItem(EDITOR_WINDOW_MENU_ITEM, false, EDITOR_WINDOW_PRIORITY)]
+		private static void ShowWindow()
+		{
+			SelectionListDemoEditor window = GetWindow<SelectionListDemoEditor>(false, EDITOR_WINDOW_TITLE, true) as SelectionListDemoEditor;
+			window.Show();
+		}
+
+		#endregion
+
+	}
+
+}
---
+++ Editor/SelectionList.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace MuffinDev.EditorUtils
+{
+
+	///<summary>
+	/// Draws a list with selectable labels.
+	///</summary>
+	[System.Serializable]
+	public class SelectionList
+	{
+
+		#region Properties
+
+		public delegate bool SelectItemDelegate(int _Index, string _Item, int _LastSelectedIndex);
+
+		[SerializeField]
+		private List<string> m_Items = new List<string>();
+
+		[SerializeField]
+		private int m_SelectedIndex = 0;
+
+		[SerializeField]
+		private SelectItemDelegate m_OnSelectItem = null;
+
+		[SerializeField]
+		private Vector2 m_ScrollPosition = Vector2.zero;
+
+		private static GUIStyle s_ItemsListStyle = null;
+		private static GUIStyle s_SelectedItemStyle = null;
+		private static GUIStyle s_UnselectedItemStyle = null;
+
+		#endregion
+
+
+		#region Initialization
+
+		/// <summary>
+		/// Creates a SelectionList instance.
+		/// </summary>
+		public SelectionList() { }
+
+		/// <summary>
+		/// Creates a SelectionList instance, and initializes its items list with the given collection.
+		/// </summary>
+		public SelectionList(IEnumerable<string> _Items)
+        {
+			m_Items = new List<string>(_Items);
+		}
+
+		#endregion
+
+
+		#region GUI
+
+		/// <summary>
+		/// Draws the selection list using -Layout methods.
+		/// </summary>
+		public void DrawLayout()
+		{
+			DrawLayout(ItemsListStyle);
+		}
+
+		/// <summary>
+		/// Draws the selection list using -Layout methods.
+		/// </summary>
+		/// <param name="_Style">The style to use for the items list.</param>
+		public void DrawLayout(GUIStyle _Style)
+        {
+			m_ScrollPosition = EditorGUILayout.BeginScrollView(m_ScrollPosition, _Style);
+            {
+				for (int i = 0; i < Items.Count; i++)
+				{
+					DrawItemLayout(i, Items[i]);
+				}
+			}
+			EditorGUILayout.EndScrollView();
+		}
+
+		/// <summary>
+		/// Draws an item of the list.
+		/// </summary>
+		/// <param name="_Index">The index of the drawn item.</param>
+		/// <param name="_Item">The string value of the drawn item.</param>
+		private void DrawItemLayout(int _Index, string _Item)
+		{
+			bool clicked = (_Index == SelectedIndex)
+				? GUILayout.Button(_Item, SelectedItemStyle, GUILayout.ExpandWidth(true))
+				: GUILayout.Button(_Item, UnselectedItemStyle, GUILayout.ExpandWidth(true));
+
+			if (clicked)
+			{
+				SelectedIndex = _Index;
+			}
+		}
+
+		#endregion
+
+
+		#region Accessors
+
+		/// <summary>
+		/// Gets/Sets the items list.
+		/// </summary>
+		public List<string> Items
+		{
+			get { return m_Items; }
+			set { m_Items = value; }
+		}
+
+		/// <summary>
+		/// Gets/Sets the index of the selected item.
+		/// </summary>
+		private int SelectedIndex
+		{
+			get
+			{
+				if (m_SelectedIndex >= m_Items.Count)
+				{
+					int lastSelectedIndex = m_SelectedIndex;
+					m_SelectedIndex = 0;
+					if (m_OnSelectItem != null)
+						m_OnSelectItem.Invoke(m_SelectedIndex, m_Items[m_SelectedIndex], lastSelectedIndex);
+				}
+				return m_SelectedIndex;
+			}
+			set
+            {
+				int lastSelectedIndex = m_SelectedIndex;
+				m_SelectedIndex = value;
+				if(m_OnSelectItem != null)
+					m_OnSelectItem.Invoke(m_SelectedIndex, m_Items[m_SelectedIndex], lastSelectedIndex);
+            }
+		}
+
+		/// <summary>
+		/// Gets the default style of the items list.
+		/// </summary>
+		public static GUIStyle ItemsListStyle
+		{
+			get
+			{
+				if (s_ItemsListStyle == null)
+				{
+					s_ItemsListStyle = new GUIStyle("OL box");
+					s_ItemsListStyle.padding = new RectOffset();
+					s_ItemsListStyle.margin = new RectOffset(6, 6, 6, 6);
+				}
+				return s_ItemsListStyle;
+			}
+		}
+
+		/// <summary>
+		/// Gets the style for selected items.
+		/// </summary>
+		public static GUIStyle SelectedItemStyle
+		{
+			get
+			{
+				if (s_SelectedItemStyle == null)
+				{
+					s_SelectedItemStyle = new GUIStyle("OL SelectedRow");
+					s_SelectedItemStyle.padding = new RectOffset(6, 4, 4, 4);
+					s_SelectedItemStyle.margin = new RectOffset();
+				}
+				return s_SelectedItemStyle;
+			}
+		}
+
+		/// <summary>
+		/// Gets the style for unselected items.
+		/// </summary>
+		public static GUIStyle UnselectedItemStyle
+		{
+			get
+			{
+				if (s_UnselectedItemStyle == null)
+				{
+					s_UnselectedItemStyle = new GUIStyle(EditorStyles.label);
+					s_UnselectedItemStyle.padding = new RectOffset(6, 4, 4, 4);
+					s_UnselectedItemStyle.margin = new RectOffset();
+				}
+				return s_UnselectedItemStyle;
+			}
+		}
+
+		#endregion
+
+	}
+
+}
